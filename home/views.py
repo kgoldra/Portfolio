3d1d23051f1d7e960da25b9ec1c46046dc7bf6b9
kgoldra <kgoldra@gmail.com>
2023-05-08 17:12:18 +0700

    creating a different ui

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home_screen_view(request):
     
     return render(request, "home/home.html", {})
+
+def test_function(request):
+    request.navBar
